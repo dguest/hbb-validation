7da78cb746135275567f375a06dda2dded71b1ec
Dan Guest <dguest@cern.ch>
2018-03-19 22:25:12 +0100

    bug fix in denom getter script

--- get_weight_demominator.py
+++ get_weight_demominator.py
@@ -8,7 +8,7 @@ from argparse import ArgumentParser
 from h5py import File
 from glob import glob
 from collections import Counter
-from common import get_dsid
+from common import get_dsid, is_dijet
 
 import sys
 import json
@@ -29,15 +29,14 @@ def run():
     counts = Counter()
     for ds in args.datasets:
         dsid = get_dsid(ds)
-        is_dijet = is_dijet(dsid)
         for fpath in glob(f'{ds}/*.h5'):
-            counts[dsid] += get_counts(fpath, is_dijet)
+            counts[dsid] += get_counts(fpath, is_dijet=is_dijet(dsid))
 
     if args.out_file:
         out = open(args.out_file, 'w')
     else:
         out = sys.stdout
-    json.dump(dict(counts), sys.stdout, indent=2)
+    json.dump(dict(counts), out, indent=2)
 
 if __name__ == '__main__':
     run()
