fcb048bbf6ae6c229dcbf00d02a55f1e63547ad4
Daniel Guest <dguest@cern.ch>
2018-08-13 03:49:56 -0700

    Get truncated file list script working

--- xbb_make_truncated_file_list.py
+++ xbb_make_truncated_file_list.py
@@ -16,15 +16,18 @@ from h5py import File
 from glob import glob
 from os import symlink, mkdir, makedirs
 from pathlib import Path
+from os.path import relpath
 
 def get_args():
     parser = ArgumentParser(description=__doc__)
     d = 'default: %(default)s'
-    parser.add_argument('datasets', nargs='+', help=_dataset_help)
+    parser.add_argument('datasets', nargs='+', help=_dataset_help, type=Path)
     parser.add_argument(
-        '-o','--output-dir', help="build new file tree here", required=True)
+        '-o','--output-dir', help="build new file tree here", required=True,
+        type=Path)
     parser.add_argument('-m','--max_entries', type=int,
                         default=1_000_000, help=d)
+    parser.add_argument('-v', '--verbose', action='store_true')
     return parser.parse_args()
 
 def run():
@@ -32,14 +35,21 @@ def run():
     makedirs(args.output_dir)
     for ds in args.datasets:
         ds_count = 0
-        outdir_path = Path(args.output_dir, Path(ds).name)
+        outdir_path = Path(args.output_dir, ds.name)
         mkdir(outdir_path)
-        for fpath in sorted(glob('f{ds}/*.h5')):
+        if args.verbose:
+            print(f'adding {ds.name}')
+        for fpath in sorted(ds.glob('*.h5')):
             if ds_count > args.max_entries:
+                if args.verbose:
+                    print(f'added {ds_count:,} entries to {ds.name}, stopping')
                 break
-            old_file = Path(fpath).resolved()
-            new_file = Path(outdir_path, old_file.name)
-            
+            old_file = Path(fpath).resolve()
+            new_file = outdir_path.joinpath(old_file.name).resolve()
+            with File(old_file, 'r') as h5file:
+                ds_count += h5file['fat_jet'].shape[0]
+            link_path = relpath(old_file, new_file.parent)
+            symlink(link_path, new_file)
 
 if __name__ == '__main__':
     run()
