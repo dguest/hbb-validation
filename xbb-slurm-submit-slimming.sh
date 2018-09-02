d1aa43bc9d4c5f9b74cc9cd71856eb4d517d7613
Daniel Guest <dguest@cern.ch>
2018-09-02 13:20:01 -0700

    Make a better batch sub script

--- xbb-slurm-submit-slimming.sh
+++ xbb-slurm-submit-slimming.sh
@@ -2,8 +2,74 @@
 
 set -eu
 
+_usage() {
+    echo "usage: ${0##*/} [-hf] -i <inputs> [-o outputs] [-l logs]"
+}
+
+OUTPUTS=slimmed
+INPUTS=''
+LOGS=logs
+
+_help() {
+    _usage
+    cat <<EOF
+
+Submit some batch jobs to slim things
+
+Options:
+ -h: get help
+ -f: delete existing files
+ -i <input_dir>: directory where the input datasets live
+ -o <output_dir>: where the outputs will go, default "${OUTPUTS}"
+ -l <logs_dir>: where to put logs, default "${LOGS}"
+
+EOF
+}
+
+FORCE=''
+
+get_options() {
+    local opt
+    while getopts ":hfi:o:" opt $@; do
+	      case $opt in
+	          h) _help; return 1;;
+            f) FORCE=TRUE ;;
+	          i) INPUTS=${OPTARG} ;;
+            o) OUTPUTS=${OPTARG} ;;
+            l) LOGS=${OPTARG} ;;
+	          # handle errors
+	          \?) _usage; echo "Unknown option: -$OPTARG" >&2; exit 1;;
+            :) _usage; echo "Missing argument for -$OPTARG" >&2; exit 1;;
+            *) _usage; echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+	      esac
+    done
+    shift $(($OPTIND - 1))
+
+    if [[ -z $INPUTS || ! -d ${INPUTS} ]] ; then
+        echo "Missing inputs dir" >&2
+        exit 1
+    fi
+
+}
+
+# __________________________________________________________________
+# run all the things
+
+# get options
+get_options $@
+
+if [[ -d ${OUTPUTS} ]]; then
+    if [[ $FORCE ]]; then
+        echo "removing ${OUTPUTS}"
+        rm -r ${OUTPUTS}
+    else
+        echo "found ${OUTPUTS}, refusing to overwrite" >&2
+        exit 1
+    fi
+fi
+
 # make output log directory
-mkdir -p logs
+mkdir -p ${LOGS}
 
 # Set the options. You can get a lot more information bo looking at
 # the help! Just look at "sbatch -h".
@@ -16,11 +82,15 @@ mkdir -p logs
 # The '-o' and '-e' are to specify where the outputs go. Note that the
 # time limit, '-t' is in minutes.
 #
-BOPTS="-t 60 -p atlas_all -o logs/out-%j.txt -e logs/error-%j.txt"
+BOPTS="-t 60 -p atlas_all -o ${LOGS}/out-%j.txt -e ${LOGS}/error-%j.txt"
 
 # Actually submit the job
 #
-for DS in $1/*
+for DS in $INPUTS/*
 do
-    sbatch ${BOPTS} xbb-slurm-run-slimming.sh $DS $2
+    if [[ ! -d $DS ]] ; then
+        echo "skipping $DS, it is not a directory!" >&2
+        continue
+    fi
+    sbatch ${BOPTS} xbb-slurm-run-slimming.sh $DS $OUTPUTS
 done
